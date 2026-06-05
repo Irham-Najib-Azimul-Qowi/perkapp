7d0e296968b635942a584a57a80b126f23a08493
adamlki <adambchbeleng2@gmail.com>
2026-06-05 13:05:53 +0700

    menambahkan home screen kegiatan ui

---
+++ app/src/main/java/com/example/perkapp/features/kegiatan/ui/HomeScreen.kt
@@ -0,0 +1,24 @@
+package com.example.perkapp.features.kegiatan.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Halaman Kegiatan\n(Dikerjakan Reja)",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = androidx.compose.ui.text.style.TextAlign.Center
+        )
+    }
+}
