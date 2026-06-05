869fd31f4b6f69e3d978920692c36c29103367b2
adamlki <adambchbeleng2@gmail.com>
2026-06-05 13:06:45 +0700

    menambahkan splashscreen

---
+++ app/src/main/java/com/example/perkapp/features/auth/ui/SplashScreen.kt
@@ -0,0 +1,55 @@
+package com.example.perkapp.features.auth.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.perkapp.core.datastore.UserPreferences
+import com.example.perkapp.core.datastore.dataStore
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    onNavigateToHome: () -> Unit,
+    onNavigateToLogin: () -> Unit
+) {
+    val context = LocalContext.current
+    val userPreferences = remember { UserPreferences(context.dataStore) }
+    // Gunakan initial state "LOADING" sebagai indikator kita masih mengecek datastore
+    val token by userPreferences.getAuthToken.collectAsState(initial = "LOADING")
+
+    LaunchedEffect(token) {
+        // Jika token masih "LOADING", artinya kita belum selesai membaca DataStore. Kita tunggu.
+        if (token != "LOADING") {
+            // Beri sedikit delay agar SplashScreen sempat terlihat oleh mata user
+            delay(1500)
+            
+            if (token.isNullOrEmpty()) {
+                // Token tidak ada, user harus login
+                onNavigateToLogin()
+            } else {
+                // Token sudah ada, bypass login (langsung masuk home)
+                onNavigateToHome()
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "SIEPERKAP",
+            style = MaterialTheme.typography.displayMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
